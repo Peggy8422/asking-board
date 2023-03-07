a6c7008a6ea7fa4b2b4973b5d104ca3028c97305
Peggy8422 <pig2222pig222@gmail.com>
2023-03-07 23:04:32 +0800

    refactor: 修改登入頁面驗證欄位

--- src/pages/AdminLoginPage.tsx
+++ src/pages/AdminLoginPage.tsx
@@ -6,7 +6,7 @@ const AdminLoginPage = () => {
   return (
     <Box w={'100%'} h={'100vh'} bg={'brand.400'}>
       <AuthForm isUser={false} isOnRegist={false}>
-        <AuthInput label='帳號' type='text' placeholder='請輸入帳號' />
+        <AuthInput label='Email' type='email' placeholder='請輸入Email' />
         <AuthInput label='密碼' type='password' placeholder='請輸入密碼' />
       </AuthForm>
     </Box>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,7 +8,7 @@ const LoginPage = () => {
   return (
     <Box w={'100%'} h={'100vh'} bg={'brand.400'}>
       <AuthForm isOnRegist={false} isUser={true}>
-        <AuthInput label='帳號' type='text' placeholder='請輸入帳號' />
+        <AuthInput label='Email' type='email' placeholder='請輸入Email' />
         <AuthInput label='密碼' type='password' placeholder='請輸入密碼' />
       </AuthForm>
       <Tooltip label={'前往管理者登入'} placement={'top'} >
