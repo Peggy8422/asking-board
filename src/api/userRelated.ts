9d5d81e76138d604331bdb73e3c103ded0edbe5e
Peggy8422 <pig2222pig222@gmail.com>
2023-04-05 23:00:50 +0800

    test: 測試環境變數.env問題

--- src/api/userRelated.ts
+++ src/api/userRelated.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASEURL;
+// const baseURL = process.env.REACT_APP_API_BASEURL;
+const baseURL = 'https://t7gnwvvq9h.execute-api.ap-northeast-1.amazonaws.com/api/v1';
 
 //修改個人資料相關
 //修改當前使用者帳戶資訊
