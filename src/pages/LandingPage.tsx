4ee100a6e5c413fbed10cd6bc986c8e86df8465b
Peggy8422 <pig2222pig222@gmail.com>
2023-04-12 23:23:23 +0800

    fix: 修改LandingPage按鈕在手機板被覆蓋按不到的問題

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import LandingPanel from "../components/LandingPanel";
+import LandingPanel from '../components/LandingPanel';
 import { Container, Box } from '@chakra-ui/react';
 import { QuestionIllus, TeacherIllus } from '../assets/images';
-
 
 const LandingPage = () => {
   return (
     <Box w={'100%'} h={'100vh'} overflowY={'hidden'}>
-      <Container maxW={'container.lg'} h={'100vh'} py={'40'} position={'relative'}>
-        <LandingPanel />
-        <Box position={'absolute'} top={{base: '20', md: '32'}} right={{base: '15%', md: '20%'}} width={'10vw'} zIndex={-1}>
-          <QuestionIllus width={'100%'}/>
+      <Container
+        maxW={'container.lg'}
+        h={'100vh'}
+        py={'40'}
+        position={'relative'}
+      >
+        <Box
+          position={'absolute'}
+          bottom={{ base: '-150', md: '-20' }}
+          right={'0'}
+          width={{ base: '85vw', md: '40vw' }}
+        >
+          <TeacherIllus width={'100%'} />
         </Box>
-        <Box position={'absolute'} bottom={{base: '-150', md: '-20'}} right={'0'} width={{base: '85vw', md: '40vw'}}>
-          <TeacherIllus width={'100%'}/>
+        <LandingPanel />
+        <Box
+          position={'absolute'}
+          top={{ base: '20', md: '32' }}
+          right={{ base: '15%', md: '20%' }}
+          width={'10vw'}
+          zIndex={-1}
+        >
+          <QuestionIllus width={'100%'} />
         </Box>
       </Container>
     </Box>
